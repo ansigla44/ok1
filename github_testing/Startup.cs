ab44a8343e527d9790d7dcbd82689cb698260c7f
ansigla <ansigla44@gmail.com>
2020-10-31 10:05:52 +0530

    done

---
+++ github_testing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(github_testing.Startup))]
+namespace github_testing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
